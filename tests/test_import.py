ea3754aef679f35fa747320870a2c134a9db3c9b
Karthikeya Kona <konakarthik12@gmail.com>
2025-01-26 11:10:13 -0500

    add more import paths

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,12 +6,15 @@ import omni.isaac.kit
 import carb
 import omni.isaac.lab.sim
 import pegasus.simulator.logic
+import omni.isaac.version
+
 modules = [
     omni.isaac.kit,
     carb,
     omni.isaac.lab.sim,
     pegasus.simulator.logic,
-    omni.isaac.dynamic_control
+    omni.isaac.dynamic_control,
+    omni.isaac.version
 
 ]
 for module in modules:
