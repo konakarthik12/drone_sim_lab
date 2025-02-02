b60a71502eb757c209d7a49ac2576ea54e9d07e9
Karthikeya Kona <konakarthik12@gmail.com>
2025-02-02 14:11:17 -0500

    add video recording during replay

--- standalone/drone_teleop_replay.py
+++ standalone/drone_teleop_replay.py
@@ -2,14 +2,18 @@ from sim.app import init_app
 
 init_app()
 
+from sim.video_recorder import VideoRecorder
 from pandas import read_pickle
 from drone.drone_env import DroneEnv
 
 env = DroneEnv(layout_type="grid")
 
-commands = read_pickle("drone_commands.pkl")
-print(len(commands))
 commands = read_pickle("episode_actions.pkl")
+video_recorder = VideoRecorder("/World/drone/arm/arm_base/Camera", fps=30)
 env.reset()
 for action in commands:
     env.step(action)
+
+video_path = "episode.mp4"
+video_recorder.save(video_path)
+print("Video saved to", video_path)
