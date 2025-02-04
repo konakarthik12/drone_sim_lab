2ccdf4a7a6dbde3b9c3e25dffecb3aac6f070808
Karthikeya Kona <konakarthik12@gmail.com>
2025-02-04 17:41:59 -0500

    switch back to playing in grid environment

--- rl_games_helper/locomotion_env.py
+++ rl_games_helper/locomotion_env.py
@@ -25,7 +25,7 @@ class LocomotionEnv(IsaacEnv, gym.Env):
 
     def __init__(self, cfg: AntEnvCfg, **kwargs):
 
-        super().__init__(layout_type="air")
+        super().__init__(layout_type="grid")
 
         # store inputs to class
         self.cfg = cfg
