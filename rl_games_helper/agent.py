c628ea140953efe1d09fa842bc9bea7295482a3d
Karthikeya Kona <konakarthik12@gmail.com>
2025-02-05 00:16:03 -0500

    merge rl register into rl agent file

--- rl_games_helper/agent.py
+++ rl_games_helper/agent.py
@@ -1,15 +1,38 @@
+import gym.spaces  # needed for rl-games incompatibility: https://github.com/Denys88/rl_games/issues/261
 import omni.isaac.lab_tasks  # noqa: F401
+from rl_games.common import env_configurations
 from rl_games.common.player import BasePlayer
+from rl_games.common.vecenv import IVecEnv
 from rl_games.torch_runner import Runner
 
 from rl_games_helper.locomotion_env import LocomotionEnv
-from rl_games_helper.rl_games_raw import register_rl_games_env
+
+
+def register_rl_games_env(observation_space, action_space):
+    class RlGamesVecEnvWrapper(IVecEnv):
+
+        def __init__(self, ):
+            # store provided arguments
+            self._rl_device = "cpu"
+            self._clip_obs = float('inf')
+            self._clip_actions = 1.0
+            self._sim_device = "cpu"
+
+            self.observation_space = gym.spaces.Box(-self._clip_obs, self._clip_obs, observation_space.shape)
+            env_configurations.register("rlgpu",
+                                        {"vecenv_type": "IsaacRlgWrapper", "env_creator": lambda **kwargs: self})
+
+            self.action_space = gym.spaces.Box(-self._clip_actions, self._clip_actions, action_space.shape)
+
+
+    env_configurations.register("rlgpu", {"vecenv_type": "IsaacRlgWrapper",
+                                              "env_creator": lambda **kwargs: RlGamesVecEnvWrapper()})
 
 
 class Agent:
     def __init__(self, env: LocomotionEnv, agent_cfg, resume_path, num_envs=1):
         # wrap around environment for rl-games
-        register_rl_games_env(env)
+        register_rl_games_env(env.observation_space, env.action_space)
         # load previously trained model
         agent_cfg["params"]["load_checkpoint"] = True
         agent_cfg["params"]["load_path"] = resume_path
