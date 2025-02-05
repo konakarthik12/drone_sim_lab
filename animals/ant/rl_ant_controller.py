9445f89019b589f9735d60b53f316d9f8571e051
Karthikeya Kona <konakarthik12@gmail.com>
2025-02-04 23:23:39 -0500

    lighting handled inside usd file, not ant controller

--- animals/ant/rl_ant_controller.py
+++ animals/ant/rl_ant_controller.py
@@ -35,8 +35,6 @@ class RlAntController(AntController):
 
         # instantiate actions (needed for tasks for which the observations computation is dependent on the actions)
         self.actions = sample_space(self.env.single_action_space, self.sim.device, batch_size=1, fill_value=0)
-        light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
-        light_cfg.func("/World/Light", light_cfg)
     def post_init(self):
         self._joint_dof_idx, _ = self.robot.find_joints(".*")
 
