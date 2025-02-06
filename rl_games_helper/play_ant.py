2d9f6c4e87c697fcc04d7d4ab462f675d551efff
Karthikeya Kona <konakarthik12@gmail.com>
2025-02-05 23:51:09 -0500

    clean up drone env in ant play

--- rl_games_helper/play_ant.py
+++ rl_games_helper/play_ant.py
@@ -43,54 +43,26 @@ class AntEnv(IsaacEnv):
 env_cfg = AntEnvCfg()
 env = AntEnv(env_cfg)
 
+from drone.drone_controller_qgroundcontrol import DroneControllerQGroundControl
 
 # create drone controller
-# drone_controller = DroneControllerQGroundControl(env)
-
-
-def sha1_array(arr: Tensor):
-    import hashlib
-
-    # Ensure tensor is in CPU memory and convert to NumPy
-    from numpy import ndarray
-    tensor_np: ndarray = arr.detach().cpu().numpy()
-
-    # Convert tensor to bytes
-    tensor_bytes = tensor_np.tobytes()
-
-    # Compute SHA-1 hash
-    sha1_hash = hashlib.sha1(tensor_bytes).hexdigest()
-
-    return sha1_hash
-
+drone_controller = DroneControllerQGroundControl(env)
 
 set_active_camera("/World/drone/arm/arm_base/Camera")
 
 mani_state = ManipulatorState()
 add_gamepad_callback(mani_state.gamepad_callback)
-# reset environment
 env.reset()
-
-# simulate environment
-# note: We simplified the logic in rl-games player.py (:func:`BasePlayer.run()`) function in an
-#   attempt to have complete control over environment stepping. However, this removes other
-#   operations such as masking that is used for multi-agent learning by RL-Games.
-count = 0
-
+drone_controller.post_init()
 while app.is_running():
 
-    # drone_controller.step(mani_state.as_action())
-    count += 1
-    if count == 4:
-        pass
-        # assert sha1_array(env.ant_controller.last_obs) == "3cbeb8f5a1e73b228b90ffdbca2a073b0557bedd"
+    drone_controller.step(mani_state.as_action())
     for _ in range(4):
         env.step(None)
 
     env.sim.render()
     obs = env.ant_controller.last_obs
 
-last_sha = sha1_array(env.ant_controller.last_obs)
 # close the simulator
 env.close()
 
